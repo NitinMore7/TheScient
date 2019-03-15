e8b91bd20a4d9bfa35415f1f3e9fd050ac744974
rarsahki <arksri14@gmail.com>
2019-03-16 02:26:32 +0530

    Announcement created

--- app/src/main/java/com/example/lenovo/thescient/Announcement.java
+++ app/src/main/java/com/example/lenovo/thescient/Announcement.java
@@ -6,13 +6,33 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Announcement extends AppCompatActivity {
     BottomSheetBehavior bottomSheetBehavior;
+    AnnouncementAdapter announcementAdapter;
+    ArrayList<Announcement_objects> arrayList;
+    String url = "https://scient.nitt.edu/announcements";
+    RecyclerView recyclerView;
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +48,11 @@ public class Announcement extends AppCompatActivity {
             });
         }else{
             setContentView(R.layout.activity_announcement);
-            final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.acitivity_announcements);
+            arrayList = new ArrayList<>();
+            recyclerView = (RecyclerView) findViewById(R.id.recycler_event);
+            requestQueue = Volley.newRequestQueue(this);
+            jsonParse();
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }
         ImageView home = (ImageView) findViewById(R.id.Home);
         home.setOnClickListener(new View.OnClickListener() {
@@ -135,6 +159,32 @@ public class Announcement extends AppCompatActivity {
                 }
             }
         });
+    }
+    public void jsonParse(){
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("announcements");
+                            for(int i = 0; i < jsonArray.length(); i++){
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                String details = jsonObject.getString("Text");
+                                announcementAdapter = new AnnouncementAdapter(getBaseContext(),arrayList);
+                                recyclerView.setAdapter(announcementAdapter);
+                                arrayList.add(new Announcement_objects(details));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
     }
     @Override
     protected void onStop() {
