c2cbb0d3f52de844e792589d1aea3547cf32a665
Vaibhav Bairagi <vaibhavbairagi105@gmail.com>
2019-03-20 18:35:16 +0530

    modified Project Registration Activity, added add material and add service vies, modified rpro post method, modified Register Activity

--- app/src/main/java/com/example/lenovo/thescient/Rproject.java
+++ app/src/main/java/com/example/lenovo/thescient/Rproject.java
@@ -15,6 +15,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -26,17 +28,29 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Rproject extends AppCompatActivity {
-EditText name,roll,dept,cno,email,visibility,abstrac,budget,timeline;
+EditText name,roll,dept,cno,email,abstrac,budget,timeline;
     BottomSheetBehavior bottomSheetBehavior;
-    Button addmemberbutton;
+    Button addmemberbutton,addmaterialsbutton,addservicebutton;
 Button submitt;public enum visi{open,closed}
-LinearLayout.LayoutParams layoutParams;
-ArrayList<View> viewlist=new ArrayList<>();
+ArrayList<View> addmemberviewlist=new ArrayList<>(),addmaterialviewlist=new ArrayList<>(),addserviceviewlist=new ArrayList<>();
 LinearLayout addmemberlayout;
+String radioText;
+    ArrayList<String> teamMembersNames=new ArrayList<>();
+    ArrayList<String> teamMembersRoll=new ArrayList<>();
+    ArrayList<String> materialNames=new ArrayList<>();
+    ArrayList<String> materialSpecs=new ArrayList<>();
+    ArrayList<String> materialQuantity=new ArrayList<>();
+    ArrayList<String> materialPrice=new ArrayList<>();
+    ArrayList<String> purpose=new ArrayList<>();
+    ArrayList<String> vendors=new ArrayList<>();
+    ArrayList<String> serviceNames=new ArrayList<>();
+    ArrayList<String> serviceSpecs=new ArrayList<>();
+    ArrayList<String> servicePrice=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_rproject);
         final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_rproject);
         ImageView back = findViewById(R.id.Home);
@@ -53,26 +67,67 @@ LinearLayout addmemberlayout;
         dept=(EditText)findViewById(R.id.edt_deptr);
         cno=(EditText)findViewById(R.id.edt_Cnor);
         email=(EditText)findViewById(R.id.edt_emailr);
-        visibility=(EditText)findViewById(R.id.edt_visibilityr);
+        addmaterialsbutton=findViewById(R.id.add_material_button);
         abstrac=(EditText)findViewById(R.id.edt_abstractr);
         budget=(EditText)findViewById(R.id.edt_budgetr);
         timeline=(EditText)findViewById(R.id.edt_timeliner);
         addmemberbutton=findViewById(R.id.add_member_button);
         addmemberlayout=findViewById(R.id.add_edit_text_layout);
+        addservicebutton=findViewById(R.id.add_service_button);
+
+
+
         addmemberbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //addview
                 LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 final View v= Objects.requireNonNull(vi).inflate(R.layout.team_members_view,null);
                 final ViewGroup insertPoint = (ViewGroup) findViewById(R.id.add_edit_text_layout);
                 insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-                viewlist.add(v);
+                addmemberviewlist.add(v);
                 Button button=v.findViewById(R.id.close);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        viewlist.remove(v);
+                        addmemberviewlist.remove(v);
+                        insertPoint.removeView(v);
+                    }
+                });
+            }
+        });
+
+        addmaterialsbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View v= Objects.requireNonNull(vi).inflate(R.layout.materials_view,null);
+                final ViewGroup insertPoint = (ViewGroup) findViewById(R.id.add_material_child);
+                insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                addmaterialviewlist.add(v);
+                Button button=v.findViewById(R.id.close);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        addmaterialviewlist.remove(v);
+                        insertPoint.removeView(v);
+                    }
+                });
+            }
+        });
+
+        addservicebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View v= Objects.requireNonNull(vi).inflate(R.layout.service_view,null);
+                final ViewGroup insertPoint = (ViewGroup) findViewById(R.id.add_service_child);
+                insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                addserviceviewlist.add(v);
+                Button button=v.findViewById(R.id.close);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        addserviceviewlist.remove(v);
                         insertPoint.removeView(v);
                     }
                 });
@@ -87,26 +142,21 @@ LinearLayout addmemberlayout;
                 String depta=dept.getText().toString();
                 String cnoa=cno.getText().toString();
                 String emaila=email.getText().toString();
-                visibility.setHint("Open/Closed");
-                String visibi=visibility.getText().toString();
-                visi visibilitya = visi.open;
+                RadioGroup radioGroup=findViewById(R.id.radioGroup);
+                RadioButton radioButton=findViewById(radioGroup.getCheckedRadioButtonId());
+                radioText= (String) radioButton.getText();
+                visi visibilitya;
 
-
-                if(visibi.compareToIgnoreCase("open")==1||visibi.compareToIgnoreCase("closed")==1) {
-                    if (visibi == "open")
+                    if (radioText.equals("Open"))
                         visibilitya = visi.open;
                     else
                         visibilitya = visi.closed;
-                }
-                else {
-                    visibility.setError("Please Enter a valid Text");
-                }
 
-                Editable abstraca=abstrac.getText();
+                String abstraca= String.valueOf(abstrac.getText());
                 Integer budgetr=null;
                 try{
                 budgetr=Integer.parseInt(budget.getText().toString());}catch (Exception e){}
-                Editable timeliner= timeline.getText();
+                String timeliner= String.valueOf(timeline.getText());
                 if(namea.isEmpty())
                 {
                     name.setError("Name Required");
@@ -127,16 +177,41 @@ LinearLayout addmemberlayout;
                 {
                     email.setError("Email Id is Required");
                 }
-                else if(visibilitya.toString().isEmpty())
-                {
-                    visibility.setError("Purpose Required");
+                else if(checkemptyteamViews()){
+                    Toast.makeText(getApplicationContext(),"Please fill up all details of team members",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(checkemptymaterialViews()){
+                    Toast.makeText(getApplicationContext(),"Please fill up all details of materials",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(checkEmptyServiceViews()){
+                    Toast.makeText(getApplicationContext(),"Please fill up all details of Services",Toast.LENGTH_SHORT).show();
                 }
 
                 else {
+                    for(int i=0;i<addmemberviewlist.size();i++){
+                        teamMembersNames.add(((EditText)addmemberviewlist.get(i).findViewById(R.id.member_name)).getText().toString());
+                        teamMembersRoll.add(((EditText)addmemberviewlist.get(i).findViewById(R.id.member_rollno)).getText().toString());
+                    }
+                    for(int i=0;i<addmaterialviewlist.size();i++){
+                        materialNames.add(((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_name)).getText().toString());
+                        materialSpecs.add(((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_specification)).getText().toString());
+                        materialQuantity.add(((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_quantity)).getText().toString());
+                        materialPrice.add(((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_price)).getText().toString());
+                        purpose.add(((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_purpose)).getText().toString());
+                        vendors.add(((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_link)).getText().toString());
+                    }
+                    for(int i=0;i<addserviceviewlist.size();i++){
+                        serviceNames.add(((EditText)addserviceviewlist.get(i).findViewById(R.id.service_name)).getText().toString());
+                        serviceSpecs.add(((EditText)addserviceviewlist.get(i).findViewById(R.id.service_specification)).getText().toString());
+                        servicePrice.add(((EditText)addserviceviewlist.get(i).findViewById(R.id.service_price)).getText().toString());
+                    }
                     Call<ResponseBody> call=Rettrofitclient
                             .getInstance()
                             .getApi()
-                            .rpro(namea,rolla,depta,cnoa,emaila,visibilitya,abstraca,budgetr,timeliner);
+                            .rpro(namea,rolla,depta,cnoa,emaila,visibilitya,abstraca,budgetr,timeliner,teamMembersNames,teamMembersRoll,materialNames,materialSpecs,
+                                    materialQuantity,materialPrice,purpose,vendors,serviceNames,serviceSpecs,servicePrice);
 
                     call.enqueue(new Callback<ResponseBody>() {
                         @Override
@@ -161,4 +236,41 @@ LinearLayout addmemberlayout;
             }
         });
     }
+
+    boolean checkemptyteamViews(){
+        for(int i=0;i<addmemberviewlist.size();i++){
+            if(((EditText)addmemberviewlist.get(i).findViewById(R.id.member_name)).getText().toString().isEmpty() ||
+                    ((EditText)addmemberviewlist.get(i).findViewById(R.id.member_rollno)).getText().toString().isEmpty()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean checkemptymaterialViews(){
+        for(int i=0;i<addmaterialviewlist.size();i++){
+            if(((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_name)).getText().toString().isEmpty() ||
+                    ((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_specification)).getText().toString().isEmpty() ||
+                    ((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_price)).getText().toString().isEmpty() ||
+                    ((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_quantity)).getText().toString().isEmpty() ||
+                    ((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_purpose)).getText().toString().isEmpty() ||
+                    ((EditText)addmaterialviewlist.get(i).findViewById(R.id.material_link)).getText().toString().isEmpty()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean checkEmptyServiceViews(){
+        for(int i=0;i<addserviceviewlist.size();i++){
+            if(((EditText)addserviceviewlist.get(i).findViewById(R.id.service_name)).getText().toString().isEmpty() ||
+                    ((EditText)addserviceviewlist.get(i).findViewById(R.id.service_specification)).getText().toString().isEmpty() ||
+                    ((EditText)addserviceviewlist.get(i).findViewById(R.id.service_price)).getText().toString().isEmpty()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
